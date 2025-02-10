25d277cbe183b36edb26c4ddd176b0f27d8c6955
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-10 11:23:55 -0300

    v4
    Grid system

--- TicTacToeMultiplayer/Assets/Scripts/Notes.cs
+++ TicTacToeMultiplayer/Assets/Scripts/Notes.cs
@@ -7,4 +7,6 @@ public class Notes : MonoBehaviour
     // Multiplayer play mode is a mode that allows you to test your game in the editor without having to build it
     // Multiplayer tools is for check connection
     // RuntimeNetworkStatsMonitor is for see the stats of connection, server and other things
+    // We can use the the simples OnMouseDown() method to handle click events, but we can implement the interface OnPointerDownHandler to handle click events too, but if we use the interface we need to implement the method OnPointerDown and put a event system on scene, and add raycaster on the camera
+
 }
