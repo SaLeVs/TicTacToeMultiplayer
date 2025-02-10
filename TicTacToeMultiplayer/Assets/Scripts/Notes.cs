6f03db556e4ecb49e80f3e1e78e295aee47b0d45
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-10 10:47:37 -0300

    v2
    Notes + multiplayer play mode

---
+++ TicTacToeMultiplayer/Assets/Scripts/Notes.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    // Network manager works for handles starting connection, handle data and have much configs
+    // network topologies: Dedicated Game Server, Client hosted listen server, distributed authority
+
+}
