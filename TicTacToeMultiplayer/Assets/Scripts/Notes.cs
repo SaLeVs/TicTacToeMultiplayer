0b0e72b2f0df2fa03c5fdc57d0708bffd5c2cb22
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-10 10:53:34 -0300

    v3
    Import assets from CodeMonkey + Runtime network stats monitor

--- TicTacToeMultiplayer/Assets/Scripts/Notes.cs
+++ TicTacToeMultiplayer/Assets/Scripts/Notes.cs
@@ -4,5 +4,7 @@ public class Notes : MonoBehaviour
 {
     // Network manager works for handles starting connection, handle data and have much configs
     // network topologies: Dedicated Game Server, Client hosted listen server, distributed authority
-
+    // Multiplayer play mode is a mode that allows you to test your game in the editor without having to build it
+    // Multiplayer tools is for check connection
+    // RuntimeNetworkStatsMonitor is for see the stats of connection, server and other things
 }
