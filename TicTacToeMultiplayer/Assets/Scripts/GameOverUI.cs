2857efb05154dab42fe36664560ad1e1460954ae
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-11 12:21:53 -0300

    v17
    Check if tie match

--- TicTacToeMultiplayer/Assets/Scripts/GameOverUI.cs
+++ TicTacToeMultiplayer/Assets/Scripts/GameOverUI.cs
@@ -8,6 +8,7 @@ public class GameOverUI : MonoBehaviour
     [SerializeField] private TextMeshProUGUI resultTextMesh;
     [SerializeField] private Color winColor;
     [SerializeField] private Color loseColor;
+    [SerializeField] private Color tieColor;
     [SerializeField] private Button rematchButton;
 
     private void Awake()
@@ -21,7 +22,15 @@ public class GameOverUI : MonoBehaviour
     {
         GameManager.Instance.OnGameWin += GameManager_OnGameWin;
         GameManager.Instance.OnRematch += GameManager_OnRematch;
+        GameManager.Instance.OnGameTied += GameManager_OnGameTied;
         Hide();
+    }
+
+    private void GameManager_OnGameTied(object sender, EventArgs e)
+    {
+        resultTextMesh.color = tieColor;
+        resultTextMesh.text = "TIE!";
+        Show(); 
     }
 
     private void GameManager_OnRematch(object sender, EventArgs e)
