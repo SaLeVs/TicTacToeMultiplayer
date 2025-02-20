112c6b552cc45d9703ba3dd2572d406526cf4c9e
SaLeVs <alvesanicetodasilvag@gmail.com>
2025-02-20 06:32:06 -0300

    v22
    Lobby filters

--- TicTacToeMultiplayer/Assets/LobbyObj/LobbyExplanation.cs
+++ TicTacToeMultiplayer/Assets/LobbyObj/LobbyExplanation.cs
@@ -11,6 +11,9 @@ public class LobbyExplanation : MonoBehaviour
     // First step is link the project on unity services and after install the package multiplayer services
     // Second step is create a script like this and put on a gameobject
 
+    private Lobby hostLobby;
+    private float heartBeatTimer;
+
     private async void Start() // function need to be aysnc beacause this function depends off internet, if we need await, things will freeze
     {
         await UnityServices.InitializeAsync(); // we need to add this line
@@ -21,11 +24,31 @@ public class LobbyExplanation : MonoBehaviour
         };
         await AuthenticationService.Instance.SignInAnonymouslyAsync(); // we have so much options for sign in, but we can make anonymous too
 
+        ListLobbies();
         CreateLobby();
-        ListLobbies();
-
-        ListLobbies();
+        
     }
+
+    private void Update()
+    {
+        HandleLobbyHeartBeat();
+    }
+
+    private async void HandleLobbyHeartBeat()
+    {
+        if (hostLobby != null)
+        {
+            heartBeatTimer -= Time.deltaTime;
+            if (heartBeatTimer <= 0) // we can change this value
+            {
+                float heartBeatTimerMax = 15;
+                heartBeatTimer = heartBeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id); // we need to send a ping to the server to keep the lobby alive
+            }
+        }
+    }
+
 
     private async void CreateLobby()
     {
@@ -35,6 +58,7 @@ public class LobbyExplanation : MonoBehaviour
             int maxPlayers = 2;
             Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
 
+            hostLobby = lobby;
             Debug.Log("Created lobby! " + lobby.Name + " " + lobby.MaxPlayers);
         }
         catch (LobbyServiceException e)
@@ -45,17 +69,33 @@ public class LobbyExplanation : MonoBehaviour
 
     private async void ListLobbies()
     {
-        try { 
-        QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(); // here we dont have the same Lobbies reference, this change on unity 6?
-        Debug.Log("Lobbies found " + queryResponse.Results.Count);
-
-        foreach (Lobby lobby in queryResponse.Results)
+        try
         {
-            Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
-        }
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT)
+                },
+                Order = new List<QueryOrder> 
+                { 
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                }
+            };
+            QueryResponse queryResponse = await LobbyService.Instance.QueryLobbiesAsync(queryLobbiesOptions); // here we dont have the same Lobbies reference, this change on unity 6?
+            Debug.Log("Lobbies found " + queryResponse.Results.Count);
+
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
         }
         catch (LobbyServiceException e)
         {
             Debug.Log(e);
         }
+    }
+
+    // if lobby dont recieve data in 30 seconds, it will be inactive
 }
